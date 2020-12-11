4806340e8c679250e5ccc2f5141ba5816539646f
Oriana <oriana.nohemi22@gmail.com>
2020-12-11 18:07:51 -0500

    consumir funciones del servicio

--- src/app/web-speech/web-speech.component.ts
+++ src/app/web-speech/web-speech.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { defaultLanguage, languages } from '../shared/model/languages';
+import { SpeechRecognizerService } from '../shared/services/web-apis/speech-recognizer.service';
 
 @Component({
   selector: 'app-web-speech',
   templateUrl: './web-speech.component.html',
-  styleUrls: ['./web-speech.component.scss']
+  styleUrls: ['./web-speech.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WebSpeechComponent implements OnInit {
+  languages: string[] = languages;
+  currentLanguage: string = defaultLanguage;
+  totalTranscript: string;
 
-  constructor() { }
+  transcript$: Observable<string>;
+  listening$: Observable<boolean>;
+  errorMessage$: Observable<string>;
+  defaultError$ = new Subject<undefined>();
+
+  constructor(private speechRecognizer: SpeechRecognizerService) { }
 
   ngOnInit(): void {
+    this.speechRecognizer.initialize(this.currentLanguage);
+    // this.initRecognition()
   }
 
+  start(): void {
+    if(this.speechRecognizer.isListening) {
+      this.stop();
+      return;
+    }
+
+    this.defaultError$.next(undefined);
+    this.speechRecognizer.start();
+  }
+
+  stop(): void {
+    this.speechRecognizer.stop();
+  }
+
+  selectLanguage(language: string): void {
+    if(this.speechRecognizer.isListening) {
+      this.stop()
+    }
+
+    this.currentLanguage = language;
+    this.speechRecognizer.setLanguage(this.currentLanguage)
+  }
 }
