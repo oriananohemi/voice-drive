d6d9fb5864167759911b5f960d1ef55ef480f8cc
Oriana <oriana.nohemi22@gmail.com>
2020-12-11 16:46:09 -0500

    inicializar la api, seleccionar un idioma por defecto

--- src/app/shared/services/web-apis/speech-recognizer.service.ts
+++ src/app/shared/services/web-apis/speech-recognizer.service.ts
@@ -4,6 +4,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SpeechRecognizerService {
+  recognition: SpeechRecognition;
+  language: string;
+  isListening = false;
 
   constructor() { }
+
+  initialize(language: string): void {
+    this.recognition =  new webkitSpeechRecognition();
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+    this.setLanguage(language)
+  }
+
+  setLanguage(language: string): void {
+    this.language = language;
+    this.recognition.lang = language;
+  }
+  
 }
