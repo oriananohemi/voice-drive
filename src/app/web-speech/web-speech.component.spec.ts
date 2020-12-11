479917a54252b984b58fd400294378d8bbe50c0a
Oriana <oriana.nohemi22@gmail.com>
2020-12-11 15:55:32 -0500

    modulos y servicios del proyecto

---
+++ src/app/web-speech/web-speech.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WebSpeechComponent } from './web-speech.component';
+
+describe('WebSpeechComponent', () => {
+  let component: WebSpeechComponent;
+  let fixture: ComponentFixture<WebSpeechComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WebSpeechComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WebSpeechComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/web-speech/web-speech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-web-speech',
+  templateUrl: './web-speech.component.html',
+  styleUrls: ['./web-speech.component.scss']
+})
+export class WebSpeechComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
