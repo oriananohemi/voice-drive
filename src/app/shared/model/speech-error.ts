c5ebd29cd71d1d00720f02dc798b1c739834fa6e
Oriana <oriana.nohemi22@gmail.com>
2021-01-02 16:37:22 -0500

    creacion de enum con mensajes de error

--- src/app/shared/model/speech-error.ts
+++ src/app/shared/model/speech-error.ts
@@ -4,3 +4,10 @@ export enum SpeechError {
   NotAllowed = 'not-allowed',
   Unknown = 'unknown'
 };
+
+export enum SpeechErrorMessage {
+  'no-speech' = `Cannot run the demo. Your browser is not authorized to access your microphone. Verify that your browser has access to your microphone and try again.`,
+  'audio-capture' = 'No speech has been detected. Please try again.',
+  'not-allowed' = 'Microphone is not available. Plese verify the connection of your microphone and try again.',
+  'unknown' = ''
+}
